dc90b83949a010c18ed30ad4d9bace86bb142125
pacelliv <flores.eugenio03@gmail.com>
2024-03-04 15:15:22 -0500

    initial commit

---
+++ frontend-nextjs/README.md
@@ -0,0 +1,37 @@
+## Storeth Frontend
+
+### Contracts: https://github.com/pacelliv/storeth-contracts
+
+### Quickstart
+
+### Install the dependencies
+
+```bash
+yarn
+```
+
+Create a `.env.local` file with the following environment variables:
+
+```bash
+NEXT_PUBLIC_SEPOLIA_RPC_URL=https://eth-sepolia.g.alchemy.com/v2/your-api-key
+NEXT_PUBLIC_PROJECT_ID=your-project-id
+```
+
+### Start dev mode
+
+```bash
+yarn dev
+```
+
+### Build the project
+
+```bash
+yarn build && yarn start
+```
+
+### Format the project
+
+```bash
+yarn prettier:check
+yarn prettier:fix
+```
---
+++ frontend-nextjs/src/app/page.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+const Home = () => {
+    return (
+        <div className="pt-14 sm:pt-28">
+            <h1 className="mb-10 inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-center text-4xl font-extrabold text-transparent sm:max-w-2xl sm:text-left sm:text-5xl">
+                Store, manage and view your clients data.
+            </h1>
+
+            <div className="mb-24 flex flex-col items-center gap-4 sm:flex-row">
+                <Link
+                    href="databases"
+                    className="rounded-full bg-slate-700 px-6 py-3 text-base font-light text-white backdrop-blur-sm hover:bg-slate-700/90 sm:px-8 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-800/85"
+                >
+                    Start managing
+                </Link>
+                <a
+                    className="rounded-full bg-neutral-300 px-6 py-3 text-base font-normal text-zinc-950 backdrop-blur-sm hover:bg-neutral-300/80 sm:px-8 dark:bg-zinc-700 dark:text-white dark:hover:bg-zinc-700/90"
+                    href="https://github.com/pacelliv/storeth-frontend"
+                    target="_blank"
+                >
+                    Learn how it&apos;s built
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
